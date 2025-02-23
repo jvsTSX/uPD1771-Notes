eeb27fc743f9121b2dff5c9cc1c0a0acbc376f41
jvsTSX <113801355+jvsTSX@users.noreply.github.com>
2025-02-23 14:54:03 -0300

    disassembler label gen
    still need to check if the disasm files are coherent before updating the files themselves

--- dis.c
+++ dis.c
@@ -29,14 +29,36 @@ int main()
     int tmp_n, tmp_reg;
     uint16_t opcode;
 
+	uint8_t labels[512] = {0};
+
     rewind(srcptr);
     fread(&rawbin, sizeof(rawbin), 1, srcptr);
 
-    for(lpcnt = 512; lpcnt > 0; lpcnt--, idx = idx+1)
-    {
-// prepare for decode
 
-// begin decoding
+
+// it works without the label step so i assume i'm fucking up the opcode or rawbin
+
+
+	for(lpcnt = 512; lpcnt > 0; lpcnt--, ++idx) // get labels loop
+	{
+	opcode = rawbin[idx];
+    opcode = ( ((opcode >> 8) & 0x00FF) | ((opcode << 8) & 0xFF00) );
+		
+	if ((opcode & 0b1111000000000000) == 0b0110000000000000) {if ((opcode & 0b0000111111111111) < 511) labels[opcode & 0b0000000111111111] = 1;}
+	if ((opcode & 0b1111000000000000) == 0b0111000000000000) {if ((opcode & 0b0000111111111111) < 511) labels[opcode & 0b0000000111111111] = 2;}
+	}
+
+
+
+    for(lpcnt = 512, idx = 0; lpcnt > 0; lpcnt--, idx = idx+1) // decode and print loop
+    {
+	if (labels[idx] != 0)
+	{
+		if (labels[idx] == 1) fprintf(dstptr, "LOC_$%X: ", idx);
+		if (labels[idx] == 2) fprintf(dstptr, "SUB_$%X: ", idx);
+	}
+	
+	
     opcode = rawbin[idx];
 
     opcode = ( ((opcode >> 8) & 0x00FF) | ((opcode << 8) & 0xFF00) );
@@ -48,6 +70,9 @@ int main()
         {
             case 0:
                 { // misc, no fields, another switch here
+				
+				// 0000IIII----IIII - in theory 256 instructions but it decodes less than that
+				
                 switch(opcode)
                 {
                     case 0b0000000000000000: fprintf(dstptr, "nop\n");       break;
@@ -66,17 +91,17 @@ int main()
                     case 0b0000010000000100: fprintf(dstptr, "rar\n");       break;
                     case 0b0000010000001000: fprintf(dstptr, "ral\n");       break;
 
-                    case 0b0000010100000001: fprintf(dstptr, "jmpa\n");      break;
+                    case 0b0000010100000001: fprintf(dstptr, "	jmpa\n");      break;
                     case 0b0000010100000010: fprintf(dstptr, "out da\n");    break;
                     case 0b0000010100000100: fprintf(dstptr, "mul1\n");      break;
                     case 0b0000010100001100: fprintf(dstptr, "mul2\n");      break;
 
                     case 0b0000011000000100: fprintf(dstptr, "off\n");       break;
 
-                    case 0b0000100000000000: fprintf(dstptr, "ret\n");       break;
-                    case 0b0000100000000001: fprintf(dstptr, "rets\n");      break;
+                    case 0b0000100000000000: fprintf(dstptr, "	ret\n");       break;
+                    case 0b0000100000000001: fprintf(dstptr, "	rets\n");      break;
 
-                    case 0b0000100100001111: fprintf(dstptr, "reti\n");        break;
+                    case 0b0000100100001111: fprintf(dstptr, "	reti\n");        break;
 
                     default: fprintf(dstptr, "db $%08X\n", opcode); // if op unknown, just print the raw number as data byte
                 }
@@ -102,11 +127,11 @@ int main()
                     case 0b100000000001: fprintf(dstptr, "tbl0 a, (r$%X)\n", tmp_reg); break;
                     case 0b100000000010: fprintf(dstptr, "tbl0 x, (r$%X)\n", tmp_reg); break;
                     case 0b100000000100: fprintf(dstptr, "tbl0 y, (r$%X)\n", tmp_reg); break;
-                    case 0b100000001000: fprintf(dstptr, "call0 (r$%X)\n", tmp_reg);   break;
+                    case 0b100000001000: fprintf(dstptr, "	call0 (r$%X)\n", tmp_reg);   break;
                     case 0b101000000001: fprintf(dstptr, "tbl1 a, (r$%X)\n", tmp_reg); break;
                     case 0b101000000010: fprintf(dstptr, "tbl1 x, (r$%X)\n", tmp_reg); break;
                     case 0b101000000100: fprintf(dstptr, "tbl1 y, (r$%X)\n", tmp_reg); break;
-                    case 0b101000001000: fprintf(dstptr, "call1 (r$%X)\n", tmp_reg);   break;
+                    case 0b101000001000: fprintf(dstptr, "	call1 (r$%X)\n", tmp_reg);   break;
                     default: fprintf(dstptr, "db $%08X\n", opcode); // if op unknown, just print the raw number as data byte
                 }
                 break;
@@ -116,9 +141,9 @@ int main()
                 tmp_n = (opcode & 0b0000000011111111);
                 switch((opcode & 0b0001111100000000) >> 8)
                 {
-                    case 0b00000: fprintf(dstptr, "jps $%X\n", tmp_n); break;
+                    case 0b00000: fprintf(dstptr, "	jps $%X\n", tmp_n); break;
                     case 0b00001: fprintf(dstptr, "mvi md0, $%X\n", tmp_n); break;
-                    case 0b01000: fprintf(dstptr, "jmpfz $%X\n", tmp_n); break;
+                    case 0b01000: fprintf(dstptr, "	jmpfz $%X\n", tmp_n); break;
 
                     case 0b10001: fprintf(dstptr, "mvi md1, $%X\n", tmp_n); break;
                     case 0b10010: fprintf(dstptr, "mvi (h), $%X\n", tmp_n); break;
@@ -137,12 +162,12 @@ int main()
 
             case 6: // jump a12
                 tmp_n = (opcode & 0b0000111111111111);
-                fprintf(dstptr, "jmp $%X\n", tmp_n);    
+                fprintf(dstptr, "	jmp LOC_$%X\n", tmp_n);    
                 break;
 
             case 7: // call a12
                 tmp_n = (opcode & 0b0000111111111111);
-                fprintf(dstptr, "call $%X\n", tmp_n);
+                fprintf(dstptr, "	call SUB_$%X\n", tmp_n);
                 break;
 
         }
@@ -161,16 +186,16 @@ int main()
             switch((opcode & 0b0001111000000000) >> 9)
             {
                 case 0x0: fprintf(dstptr, "adi r$%X, $%X\n", tmp_reg, tmp_n); break;
-                case 0x1: fprintf(dstptr, "adis r$%X, $%X\n", tmp_reg, tmp_n); break;
+                case 0x1: fprintf(dstptr, "	adis r$%X, $%X\n", tmp_reg, tmp_n); break;
                 case 0x2: fprintf(dstptr, "sb r$%X, $%X\n", tmp_reg, tmp_n); break;
-                case 0x3: fprintf(dstptr, "sbis r$%X, $%X\n", tmp_reg, tmp_n); break;
+                case 0x3: fprintf(dstptr, "	sbis r$%X, $%X\n", tmp_reg, tmp_n); break;
                 case 0x4: fprintf(dstptr, "tadi nc r$%X, $%X\n", tmp_reg, tmp_n); break;
                 case 0x5: fprintf(dstptr, "tadi c r$%X, $%X\n", tmp_reg, tmp_n); break;
-                case 0x6: fprintf(dstptr, "tsbi nc r$%X, $%X\n", tmp_reg, tmp_n); break;
-                case 0x7: fprintf(dstptr, "tsbi c r$%X, $%X\n", tmp_reg, tmp_n); break;
+                case 0x6: fprintf(dstptr, "	tsbi nc r$%X, $%X\n", tmp_reg, tmp_n); break;
+                case 0x7: fprintf(dstptr, "	tsbi c r$%X, $%X\n", tmp_reg, tmp_n); break;
                 case 0x8: fprintf(dstptr, "adi5 r$%X, $%X\n", tmp_reg, tmp_n); break;
-                case 0x9: fprintf(dstptr, "adims r$%X, $%X\n", tmp_reg, tmp_n); break;
-                case 0xC: fprintf(dstptr, "tadi5 r$%X, $%X\n", tmp_reg, tmp_n); break;
+                case 0x9: fprintf(dstptr, "	adims r$%X, $%X\n", tmp_reg, tmp_n); break;
+                case 0xC: fprintf(dstptr, "	tadi5 r$%X, $%X\n", tmp_reg, tmp_n); break;
                 default:  fprintf(dstptr, "db $%08X\n", opcode);
             }
         }
@@ -182,18 +207,18 @@ int main()
                 case 0x1: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "and "):fprintf(dstptr, "andi "); break;
                 case 0x2: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "sb "):fprintf(dstptr, "sbi "); break;
                 case 0x3: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "or "):fprintf(dstptr, "ori "); break;
-                case 0x4: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "ads "):fprintf(dstptr, "adis "); break;
-                case 0x5: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "ands "):fprintf(dstptr, "andis "); break;
-                case 0x6: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "sbs "):fprintf(dstptr, "sbis "); break;
+                case 0x4: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	ads "):fprintf(dstptr, "	adis "); break;
+                case 0x5: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	ands "):fprintf(dstptr, "	andis "); break;
+                case 0x6: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	sbs "):fprintf(dstptr, "	sbis "); break;
                 case 0x7: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "xor "):fprintf(dstptr, "xori "); break;
-                case 0x8: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "tad nc "):fprintf(dstptr, "tadi nc "); break;
-                case 0x9: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "tand nz "):fprintf(dstptr, "tandi nz "); break;
-                case 0xA: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "tsb nc "):fprintf(dstptr, "tsbi nc "); break;
-                case 0xB: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "tsb nz "):fprintf(dstptr, "tsbi nz "); break;
-                case 0xC: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "tad c "):fprintf(dstptr, "tadi c "); break;
-                case 0xD: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "tand z "):fprintf(dstptr, "tandi z "); break;
-                case 0xE: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "tsb c "):fprintf(dstptr, "tsbi c "); break;
-                case 0xF: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "tsb z "):fprintf(dstptr, "tsbi z "); break;
+                case 0x8: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	tad nc "):fprintf(dstptr, "	tadi nc "); break;
+                case 0x9: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	tand nz "):fprintf(dstptr, "	tandi nz "); break;
+                case 0xA: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	tsb nc "):fprintf(dstptr, "	tsbi nc "); break;
+                case 0xB: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	tsb nz "):fprintf(dstptr, "	tsbi nz "); break;
+                case 0xC: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	tad c "):fprintf(dstptr, "	tadi c "); break;
+                case 0xD: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	tand z "):fprintf(dstptr, "	tandi z "); break;
+                case 0xE: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	tsb c "):fprintf(dstptr, "	tsbi c "); break;
+                case 0xF: ((opcode & 0x6000) == 0x4000)?fprintf(dstptr, "	tsb z "):fprintf(dstptr, "	tsbi z "); break;
             }
             switch (((opcode & 0b0110000000000000) >> 13))
             {
